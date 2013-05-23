2a20ae8d93b12d4fc3253e9073fb7ffe02e26987
axelduch <axlduch@gmail.com>
2013-05-23 18:20:07 +0300

    Corrected indent

--- View/Index.php
+++ View/Index.php
@@ -6,7 +6,7 @@ use Core\AbstractView as AbstractView,
 class Index extends AbstractView {
 	public function init() {
 		$this->setTemplateVar('title', 'Index');
-    	$this->addTemplate(new Template('generic_header'));
+    		$this->addTemplate(new Template('generic_header'));
 		$this->addTemplate(new Template('generic_content'));
 		$this->addTemplate(new Template('generic_footer'));
 	}
